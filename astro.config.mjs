28f1fe96199b6dda1fff9e4b7b4918a6cf4bc24a
michxymi <michxymi@gmail.com>
2026-05-06 17:33:43 +0100

    feat(ui): add more starwind components and setup theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "JetBrains Mono",
+      cssVariable: "--font-jetbrains-mono",
+      weights: [400, 500, 600],
+      styles: ["normal"],
+      fallbacks: ["monospace"],
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: [400, 500, 600],
+      styles: ["normal"],
+      fallbacks: ["sans-serif"],
+    },
+  ],
 });
